7aafc05b2b031129d886fa612467524d7a6595f0
simud04 <cambreasimone009@gmail.com>
2025-03-04 12:07:13 +0100

    Create build.gradle.kts

---
+++ src/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id 'com.android.library' // Usa 'com.android.application' se è un'applicazione e non una libreria
+    id 'kotlin-android'
+}
+
+android {
+    compileSdkVersion 30
+    defaultConfig {
+        minSdkVersion 21
+        targetSdkVersion 30
+    }
+}
+
+dependencies {
+    implementation 'org.jetbrains.kotlin:kotlin-stdlib:1.5.21' // Versione di Kotlin, assicurati che corrisponda alla tua
+    implementation "com.lagradost.cloudstream3:cloudstream3:VERSION" // Versione di Cloudstream, sostituisci con la versione che stai utilizzando
+    // Aggiungi altre dipendenze se necessario
+}
