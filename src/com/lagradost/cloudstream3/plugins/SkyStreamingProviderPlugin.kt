25e57d908335a3d39940504ec8b34e9f67095bb5
simud04 <cambreasimone009@gmail.com>
2025-03-04 12:23:40 +0100

    Update SkyStreamingProviderPlugin.kt

--- src/com/lagradost/cloudstream3/plugins/SkyStreamingProviderPlugin.kt
+++ src/com/lagradost/cloudstream3/plugins/SkyStreamingProviderPlugin.kt
@@ -1,4 +1,4 @@
-package com.github.simud04..src.com.lagradost.cloudstream3.plugins
+package com.github.simud04.src.com.lagradost.cloudstream3.plugins
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
