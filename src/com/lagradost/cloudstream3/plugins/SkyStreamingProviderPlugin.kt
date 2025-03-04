d7cc1d0740800c653b2b1a03764f0a4eab35f759
simud04 <cambreasimone009@gmail.com>
2025-03-04 12:05:35 +0100

    Create SkyStreamingProviderPlugin.kt

---
+++ src/com/lagradost/cloudstream3/plugins/SkyStreamingProviderPlugin.kt
@@ -0,0 +1,12 @@
+package com.github.simud04.providers.mail.simudsky
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+class SkyStreamingProvider : CloudstreamPlugin() {
+    override var mainUrl = "https://skystreaming.media"
+    override var name = "SkyStreaming"
+    override val hasMainPage = true
+    override val supportedTypes = setOf(TvType.Live)
+    // Altre funzioni
+}
